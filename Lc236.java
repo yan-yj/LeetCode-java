aa7a637c41121830f1d4a314b0b44951e87fa77e
yan-yj <31839720+yan-yj@users.noreply.github.com>
2022-01-11 13:04:40 +0800

    Create Lc236.java

---
+++ Lc236.java
@@ -0,0 +1,31 @@
+/**
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == p || root == q || root == null) return root;
+
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+
+        if(leftNode == null && rightNode == null){
+            return null;
+        }else if(leftNode != null && rightNode == null){
+            return leftNode;
+        }else if(leftNode == null && rightNode != null){
+            return rightNode;
+        }else {
+            return root;
+        }
+    }
+}
