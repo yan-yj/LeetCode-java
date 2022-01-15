d3dc39318a7fb774d4f1cac5fa3fb23110ca5f95
yan-yj <31839720+yan-yj@users.noreply.github.com>
2022-01-15 11:45:59 +0800

    Create Lc669.java

---
+++ Lc669.java
@@ -0,0 +1,37 @@
+/**
+Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root == null) return root;
+
+        if(root.val < low) {
+            // 直接剪去左枝，处理右枝后返回
+            return trimBST(root.right, low, high);
+        }else if(root.val > high) {
+            // 直接剪去右枝，处理左枝后返回
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+
+        return root;
+    }
+}
