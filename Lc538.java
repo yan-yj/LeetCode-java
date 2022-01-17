d78296a5bbbcbd94af2174a22af94095ef494f6f
yan-yj <31839720+yan-yj@users.noreply.github.com>
2022-01-17 10:48:12 +0800

    Create Lc538.java

---
+++ Lc538.java
@@ -0,0 +1,39 @@
+/**
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        convert(root, 0);
+        return root;
+    }
+
+    public int convert(TreeNode root, int sum) {
+
+        if(root == null) return sum;
+
+        sum = convert(root.right, sum);
+        sum += root.val;
+        root.val = sum;
+        sum = convert(root.left, sum);
+        return sum;
+    }
+}
